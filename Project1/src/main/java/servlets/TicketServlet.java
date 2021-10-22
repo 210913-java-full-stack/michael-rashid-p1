96f38d0a4ffcb28058ead5045ddab33814c3b92d
micoolio <mreece1999@gmail.com>
2021-10-22 17:01:10 -0500

    Add file logger, redesign all web pages for better navigation, add passenger manifest and the ability to cancel a passenger's ticket as an administrator, and the flight take off update for pilot. MVP is done.

--- Project1/src/main/java/servlets/TicketServlet.java
+++ Project1/src/main/java/servlets/TicketServlet.java
@@ -3,8 +3,9 @@ package servlets;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import models.*;
 import services.FlightService;
-import services.LoginService;
+import services.UserService;
 import services.TicketService;
+import utils.FileLogger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,108 +19,116 @@ import java.util.Scanner;
 public class TicketServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
     {
-        InputStream requestBody = req.getInputStream();
-        Scanner sc = new Scanner(requestBody, StandardCharsets.UTF_8.name());
-        String jsonText = sc.useDelimiter("\\A").next();
-        ObjectMapper objectMapper = new ObjectMapper();
-        Booking payload = objectMapper.readValue(jsonText,Booking.class);
+        try {
+            InputStream requestBody = req.getInputStream();
+            Scanner sc = new Scanner(requestBody, StandardCharsets.UTF_8.name());
+            String jsonText = sc.useDelimiter("\\A").next();
+            ObjectMapper objectMapper = new ObjectMapper();
+            Booking payload = objectMapper.readValue(jsonText, Booking.class);
 
-        resp.setContentType("text/plain");
-        PrintWriter out = resp.getWriter();
+            resp.setContentType("text/plain");
+            PrintWriter out = resp.getWriter();
 
-        //get the user from the username, make sure they exist
-        User currentUser = LoginService.getUserByUsername(payload.getUsername());
-        if(currentUser == null)
-        {
-            //throw exception?
-            resp.setStatus(406);
-            out.println("Username does not exist.");
-        }
-        else{
-            //checking if flight they're trying to book exists
-            Flight currentFlight = FlightService.getFlightById(payload.getFlight_id());
-            if(currentFlight != null)
-            {
-                //checking if there's enough available seats
-                if(FlightService.availableSeats(payload.getFlight_id()) >= payload.getNum_tickets())
-                {
-                    //then they can book the flight. add ticket
-                    Ticket newTicket = new Ticket(payload.getNum_tickets(),false,currentFlight,currentUser);
-                    TicketService.saveNewFlight(newTicket);
-                    currentFlight.setNum_seats(currentFlight.getNum_seats()-payload.getNum_tickets());
-                    FlightService.updateNumSeats(currentFlight);
-
-                    resp.setStatus(200);
-                    out.println("Flight booked successfully for" + currentUser.getfName() + "!");
-                }
-                else
-                {
-                    //throw exception?
-                    resp.setStatus(406);
-                    out.println("There are not enough available seats! Please try again.");
-                }
-            }
-            else
-            {
+            //get the user from the username, make sure they exist
+            User currentUser = UserService.getUserByUsername(payload.getUsername());
+            if (currentUser == null) {
                 //throw exception?
                 resp.setStatus(406);
-                out.println("That flight_id does not exist. Please try again.");
+                out.println("Username does not exist.");
+            } else {
+                //checking if flight they're trying to book exists
+                Flight currentFlight = FlightService.getFlightById(payload.getFlight_id());
+                if (currentFlight != null) {
+                    if (currentFlight.isTake_off_status()) {
+                        resp.setStatus(406);
+                        out.println("Flight has taken off already, cannot book tickets.");
+                    } else {
+                        if (FlightService.availableSeats(payload.getFlight_id()) >= payload.getNum_tickets()) {
+                            //then they can book the flight. add ticket
+                            Ticket newTicket = new Ticket(payload.getNum_tickets(), false, currentFlight, currentUser);
+                            TicketService.saveNewFlight(newTicket);
+                            currentFlight.setNum_seats(currentFlight.getNum_seats() - payload.getNum_tickets());
+                            FlightService.updateNumSeats(currentFlight);
+
+                            resp.setStatus(200);
+                            out.println("Flight booked successfully for" + currentUser.getfName() + "!");
+                        } else {
+                            //throw exception?
+                            resp.setStatus(406);
+                            out.println("There are not enough available seats! Please try again.");
+                        }
+                    }
+                } else {
+                    //throw exception?
+                    resp.setStatus(406);
+                    out.println("That flight_id does not exist. Please try again.");
+                }
             }
         }
+        catch(IOException e)
+        {
+            FileLogger.getFileLogger().console().threshold(0).writeLog("No input body to read from.", 0);
+        }
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
     {
-        InputStream requestBody = req.getInputStream();
-        Scanner sc = new Scanner(requestBody, StandardCharsets.UTF_8.name());
-        String jsonText = sc.useDelimiter("\\A").next();
-        ObjectMapper objectMapper = new ObjectMapper();
-        TicketID payload = objectMapper.readValue(jsonText, TicketID.class);
+        try {
+            InputStream requestBody = req.getInputStream();
+            Scanner sc = new Scanner(requestBody, StandardCharsets.UTF_8.name());
+            String jsonText = sc.useDelimiter("\\A").next();
+            ObjectMapper objectMapper = new ObjectMapper();
+            TicketID payload = objectMapper.readValue(jsonText, TicketID.class);
 
-        resp.setContentType("text/plain");
-        PrintWriter out = resp.getWriter();
-        //get ticket from ticket_id, make sure it exists
-        Ticket currentTicket = TicketService.getTicketById(payload.getTicket_id());
+            resp.setContentType("text/plain");
+            PrintWriter out = resp.getWriter();
+            //get ticket from ticket_id, make sure it exists
+            Ticket currentTicket = TicketService.getTicketById(payload.getTicket_id());
 
-        if(currentTicket != null)
-        {
-            //ticket exists so delete the ticket
-            TicketService.deleteTicket(currentTicket.getTicket_id());
-            resp.setStatus(200);
+            if (currentTicket != null) {
+                //ticket exists so delete the ticket
+                TicketService.deleteTicket(currentTicket.getTicket_id());
+                resp.setStatus(200);
+            } else {
+                resp.setStatus(406);
+                out.write("Tickets not found, could not delete.");
+            }
         }
-        else
+        catch(IOException e)
         {
-            resp.setStatus(406);
-            out.write("Tickets not found, could not delete.");
+            FileLogger.getFileLogger().console().threshold(0).writeLog("No input body to read from.", 0);
         }
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        InputStream requestBody = req.getInputStream();
-        Scanner sc = new Scanner(requestBody, StandardCharsets.UTF_8.name());
-        String jsonText = sc.useDelimiter("\\A").next();
-        ObjectMapper objectMapper = new ObjectMapper();
-        TicketID payload = objectMapper.readValue(jsonText, TicketID.class);
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp){
+        try {
+            InputStream requestBody = req.getInputStream();
+            Scanner sc = new Scanner(requestBody, StandardCharsets.UTF_8.name());
+            String jsonText = sc.useDelimiter("\\A").next();
+            ObjectMapper objectMapper = new ObjectMapper();
+            TicketID payload = objectMapper.readValue(jsonText, TicketID.class);
 
-        resp.setContentType("text/plain");
-        PrintWriter out = resp.getWriter();
-        //get ticket from ticket_id, make sure it exists
-        Ticket currentTicket = TicketService.getTicketById(payload.getTicket_id());
+            resp.setContentType("text/plain");
+            PrintWriter out = resp.getWriter();
+            //get ticket from ticket_id, make sure it exists
+            Ticket currentTicket = TicketService.getTicketById(payload.getTicket_id());
 
-        if(currentTicket != null)
-        {
-            //ticket exists so delete the ticket
-            TicketService.checkInForFlight(currentTicket);
-            resp.setStatus(200);
+            if (currentTicket != null) {
+                //ticket exists so delete the ticket
+                TicketService.checkInForFlight(currentTicket);
+                resp.setStatus(200);
+            } else {
+                resp.setStatus(406);
+                out.write("Tickets not found, could not check in.");
+            }
         }
-        else
+        catch(IOException e)
         {
-            resp.setStatus(406);
-            out.write("Tickets not found, could not check in.");
+            FileLogger.getFileLogger().console().threshold(0).writeLog("No input body to read from.", 0);
         }
     }
 }
